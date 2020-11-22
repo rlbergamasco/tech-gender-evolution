e6742cad73989362b882032395df6c2b558b741e
Riley Bergamasco <rlbergamasco@Rileys-MacBook-Air-3.local>
2020-11-21 22:08:43 -0500

    Added cards, javascript, and hover effects

---
+++ js/script.js
@@ -0,0 +1,162 @@
+// Get the modal
+var modal401 = document.getElementById("modal-40-1");
+
+// Get the button that opens the modal
+var img401 = document.getElementById("img-40-1");
+
+// When the user clicks on the button, open the modal
+img401.onclick = function() {
+  modal401.style.display = "block";
+}
+
+var modal402 = document.getElementById("modal-40-2");
+var img402 = document.getElementById("img-40-2");
+img402.onclick = function() {
+    modal402.style.display = "block";
+  }
+
+var modal403 = document.getElementById("modal-40-3");
+var img403 = document.getElementById("img-40-3");
+img403.onclick = function() {
+    modal403.style.display = "block";
+}
+
+var modal404 = document.getElementById("modal-40-4");
+var img404 = document.getElementById("img-40-4");
+img404.onclick = function() {
+    modal404.style.display = "block";
+}
+
+var modal501 = document.getElementById("modal-50-1");
+var img501 = document.getElementById("img-50-1");
+img501.onclick = function() {
+    modal501.style.display = "block";
+}
+
+var modal502 = document.getElementById("modal-50-2");
+var img502 = document.getElementById("img-50-2");
+img502.onclick = function() {
+    modal502.style.display = "block";
+  }
+
+var modal503 = document.getElementById("modal-50-3");
+var img503 = document.getElementById("img-50-3");
+img503.onclick = function() {
+    modal503.style.display = "block";
+}
+
+var modal601 = document.getElementById("modal-60-1");
+var img601 = document.getElementById("img-60-1");
+img601.onclick = function() {
+    modal601.style.display = "block";
+}
+
+var modal602 = document.getElementById("modal-60-2");
+var img602 = document.getElementById("img-60-2");
+img602.onclick = function() {
+    modal602.style.display = "block";
+  }
+
+var modal603 = document.getElementById("modal-60-3");
+var img603 = document.getElementById("img-60-3");
+img603.onclick = function() {
+    modal603.style.display = "block";
+}
+
+var modal604 = document.getElementById("modal-60-4");
+var img604 = document.getElementById("img-60-4");
+img604.onclick = function() {
+    modal604.style.display = "block";
+}
+
+var modal801 = document.getElementById("modal-80-1");
+var img801 = document.getElementById("img-80-1");
+img801.onclick = function() {
+    modal801.style.display = "block";
+}
+
+var modal802 = document.getElementById("modal-80-2");
+var img802 = document.getElementById("img-80-2");
+img802.onclick = function() {
+    modal802.style.display = "block";
+  }
+
+var modal803 = document.getElementById("modal-80-3");
+var img803 = document.getElementById("img-80-3");
+img803.onclick = function() {
+    modal803.style.display = "block";
+}
+
+var modal804 = document.getElementById("modal-80-4");
+var img804 = document.getElementById("img-80-4");
+img804.onclick = function() {
+    modal804.style.display = "block";
+}
+
+var modalt1 = document.getElementById("modal-t-1");
+var imgt1 = document.getElementById("img-t-1");
+imgt1.onclick = function() {
+    modalt1.style.display = "block";
+}
+
+var modalt2 = document.getElementById("modal-t-2");
+var imgt2 = document.getElementById("img-t-2");
+imgt2.onclick = function() {
+    modalt2.style.display = "block";
+  }
+
+var modalt3 = document.getElementById("modal-t-3");
+var imgt3 = document.getElementById("img-t-3");
+imgt3.onclick = function() {
+    modalt3.style.display = "block";
+}
+
+var modalt4 = document.getElementById("modal-t-4");
+var imgt4 = document.getElementById("img-t-4");
+imgt4.onclick = function() {
+    modalt4.style.display = "block";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal401) {
+      modal401.style.display = "none";
+    } else if (event.target == modal402) {
+      modal402.style.display = "none";
+    } else if (event.target == modal403) {
+      modal403.style.display = "none";
+    } else if (event.target == modal404) {
+      modal404.style.display = "none";
+    } else if (event.target == modal501) {
+      modal501.style.display = "none";
+    } else if (event.target == modal502) {
+      modal502.style.display = "none";
+    } else if (event.target == modal503) {
+      modal503.style.display = "none";
+    } else if (event.target == modal601) {
+      modal601.style.display = "none";
+    } else if (event.target == modal602) {
+      modal602.style.display = "none";
+    } else if (event.target == modal603) {
+      modal603.style.display = "none";
+    } else if (event.target == modal604) {
+      modal604.style.display = "none";
+    } else if (event.target == modal801) {
+      modal801.style.display = "none";
+    } else if (event.target == modal802) {
+      modal802.style.display = "none";
+    } else if (event.target == modal803) {
+      modal803.style.display = "none";
+    } else if (event.target == modal804) {
+      modal804.style.display = "none";
+    } else if (event.target == modalt1) {
+      modalt1.style.display = "none";
+    } else if (event.target == modalt2) {
+      modalt2.style.display = "none";
+    } else if (event.target == modalt3) {
+      modalt3.style.display = "none";
+    } else if (event.target == modalt4) {
+      modalt4.style.display = "none";
+    }
+  }
