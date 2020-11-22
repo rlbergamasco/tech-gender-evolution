2dc9bca08cc6f98bd8f1360a882c998268ab988d
Riley Bergamasco <rlbergamasco@Rileys-MacBook-Air-3.local>
2020-11-22 00:41:18 -0500

    Added javascript for stats modals

--- js/script.js
+++ js/script.js
@@ -123,6 +123,106 @@ sourcesButton.onclick = function() {
     modalSources.style.display = "block";
 }
 
+var mob41 = document.getElementById("mob41");
+var desk41 = document.getElementById("desk41");
+var stat41 = document.getElementById("stat40-1");
+mob41.onclick = function() {
+    stat41.style.display = "block";
+}
+desk41.onclick = function() {
+    stat41.style.display = "block";
+}
+
+var mob42 = document.getElementById("mob42");
+var desk42 = document.getElementById("desk42");
+var stat42 = document.getElementById("stat40-2");
+mob42.onclick = function() {
+    stat42.style.display = "block";
+}
+desk42.onclick = function() {
+    stat42.style.display = "block";
+}
+
+var mob51 = document.getElementById("mob51");
+var desk51 = document.getElementById("desk51");
+var stat51 = document.getElementById("stat50-1");
+mob51.onclick = function() {
+    stat51.style.display = "block";
+}
+desk51.onclick = function() {
+    stat51.style.display = "block";
+}
+
+var mob52 = document.getElementById("mob52");
+var desk52 = document.getElementById("desk52");
+var stat52 = document.getElementById("stat50-2");
+mob52.onclick = function() {
+    stat52.style.display = "block";
+}
+desk52.onclick = function() {
+    stat52.style.display = "block";
+}
+
+var mob61 = document.getElementById("mob61");
+var desk61 = document.getElementById("desk61");
+var stat61 = document.getElementById("stat60-1");
+mob61.onclick = function() {
+    stat61.style.display = "block";
+}
+desk61.onclick = function() {
+    stat61.style.display = "block";
+}
+
+var mob62 = document.getElementById("mob62");
+var desk62 = document.getElementById("desk62");
+var stat62 = document.getElementById("stat60-2");
+mob62.onclick = function() {
+    stat62.style.display = "block";
+}
+desk62.onclick = function() {
+    stat62.style.display = "block";
+}
+
+var mob81 = document.getElementById("mob81");
+var desk81 = document.getElementById("desk81");
+var stat81 = document.getElementById("stat80-1");
+mob81.onclick = function() {
+    stat81.style.display = "block";
+}
+desk81.onclick = function() {
+    stat81.style.display = "block";
+}
+
+var mob82 = document.getElementById("mob82");
+var desk82 = document.getElementById("desk82");
+var stat82 = document.getElementById("stat80-2");
+mob82.onclick = function() {
+    stat82.style.display = "block";
+}
+desk82.onclick = function() {
+    stat82.style.display = "block";
+}
+
+var mobt1 = document.getElementById("mobt1");
+var deskt1 = document.getElementById("deskt1");
+var statt1 = document.getElementById("statt-1");
+mobt1.onclick = function() {
+    statt1.style.display = "block";
+}
+deskt1.onclick = function() {
+    statt1.style.display = "block";
+}
+
+var mobt2 = document.getElementById("mobt2");
+var deskt2 = document.getElementById("deskt2");
+var statt2 = document.getElementById("statt-2");
+mobt2.onclick = function() {
+    statt2.style.display = "block";
+}
+deskt2.onclick = function() {
+    statt2.style.display = "block";
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -166,5 +266,25 @@ window.onclick = function(event) {
       modalt4.style.display = "none";
     } else if (event.target == modalSources) {
         modalSources.style.display = "none";
-      }
+    } else if (event.target == stat41) {
+        stat41.style.display = "none";
+    } else if (event.target == stat42) {
+        stat42.style.display = "none";
+    } else if (event.target == stat51) {
+        stat51.style.display = "none";
+    } else if (event.target == stat52) {
+        stat52.style.display = "none";
+    } else if (event.target == stat61) {
+        stat61.style.display = "none";
+    } else if (event.target == stat62) {
+        stat62.style.display = "none";
+    } else if (event.target == stat81) {
+        stat81.style.display = "none";
+    } else if (event.target == stat82) {
+        stat82.style.display = "none";
+    } else if (event.target == statt1) {
+        statt1.style.display = "none";
+    } else if (event.target == statt2) {
+        statt2.style.display = "none";
+    }
   }
